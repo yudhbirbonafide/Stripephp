329d7208505bf15c55d1d58fd1734ed22b9aee2a
yudhbirsingh <yudhbir.singh@bonafidetech.com>
2021-03-11 13:42:36 +0530

    Ecxecption handling setup

--- pay.php
+++ pay.php
@@ -41,38 +41,23 @@ try{
 } catch(Stripe_CardError $e) {
   // Since it's a decline, Stripe_CardError will be caught
   $body = $e->getJsonBody();
-  $err  = $body['error'];
-
-  print('Status is:' . $e->getHttpStatus() . "\n");
-  print('Type is:' . $err['type'] . "\n");
-  print('Code is:' . $err['code'] . "\n");
-  // param is '' in this case
-  print('Param is:' . $err['param'] . "\n");
-  print('Message is:' . $err['message'] . "\n");
+  $error = $e->getMessage();
 } catch (Stripe_InvalidRequestError $e) {
-  $body = $e->getJsonBody();
-  $err  = $body['error'];
-  $err['message'];
+  $error = $e->getMessage();
   // Invalid parameters were supplied to Stripe's API
 } catch (Stripe_AuthenticationError $e) {
-  $body = $e->getJsonBody();
-  $err  = $body['error'];
-  $err['message'];
+   $error = $e->getMessage();
   // Authentication with Stripe's API failed
   // (maybe you changed API keys recently)
 } catch (Stripe_ApiConnectionError $e) {
-  $body = $e->getJsonBody();
-  $err  = $body['error'];
-  $err['message'];
+   $error = $e->getMessage();
   // Network communication with Stripe failed
 } catch (Stripe_Error $e) {
-  $body = $e->getJsonBody();
-  $err  = $body['error'];
-  $err['message'];
+   $error = $e->getMessage();
   // Display a very generic error to the user, and maybe send
   // yourself an email
 } catch (Exception $e) {
-  $body = $e->getMessage();
+  $error = $e->getMessage();
   
   // Something else happened, completely unrelated to Stripe
 }
